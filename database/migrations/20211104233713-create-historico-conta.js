22b7427134d01d99c5f33da798043d6a79c6ffec
Joao Vitor Santos <sr.jvitor@gmail.com>
2021-11-04 21:28:50 -0300

    Adicionada configuracao do Sequelize e migrations

---
+++ database/migrations/20211104233713-create-historico-conta.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('historicoContas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      contaId: {
+        type: Sequelize.DataTypes.INTEGER,
+        references: {
+          model: {
+            tableName: 'contas'
+          },
+          key: 'id'
+        },
+        allowNull: false
+      },
+      saldo: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('historicoContas');
+  }
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppService } from './app.service';
 import { GerenciamentoContasModule } from './gerenciamento-contas/gerenciamento-contas.module';
 import { TransferenciasContasModule } from './transferencias-contas/transferencias-contas.module';
 import { BoletosBancariosModule } from './boletos-bancarios/boletos-bancarios.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [GerenciamentoContasModule, TransferenciasContasModule, BoletosBancariosModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'postgres',
+      models: [],
+    }),
+    GerenciamentoContasModule,
+    TransferenciasContasModule,
+    BoletosBancariosModule
+  ],
   controllers: [],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
